51ac12fabe3a8ef40105e654db13d1be7b02e780
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-06-09 03:09:55 +0200

    Seeing some progress, working on the admin sections r.n

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
-    return render(request, 'home/welcome.html', {}) #brackets for passing value to the view
+    return render(request, 'home/welcome.html', {"today": datetime.today()}) #brackets for passing value to the view
+
+@login_required(login_url='/admin') #restrict this page to logged in users only.
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
