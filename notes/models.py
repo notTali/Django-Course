c2789a1791f35e2c3398bfa66717f5f77c2218fe
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-06-10 02:34:41 +0200

    models

--- notes/models.py
+++ notes/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class notes(models.Model): #tell django that this class is a model
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
     
