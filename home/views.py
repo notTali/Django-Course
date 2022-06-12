df0e41dd38a8ef535df420752ce611c3fcf3e72f
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-06-12 03:31:45 +0200

    Using CBV in the home app

--- home/views.py
+++ home/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = "home/welcome.html"
+    extra_context = {"today": datetime.today()}
+
 # Create your views here.
 
-def home(request):
-    return render(request, 'home/welcome.html', {"today": datetime.today()}) #brackets for passing value to the view
+# def home(request):
+#     return render(request, 'home/welcome.html', {"today": datetime.today()}) #brackets for passing value to the view
 
 @login_required(login_url='/admin') #restrict this page to logged in users only.
 def authorized(request):
